bc1cf8629d2aeb043255e0a68c3d3f1108b5e68b
Vlad Kim <raizel@MacBook-Air-Vlad.local>
2025-05-18 16:28:17 +0900

    implemented Hibernate version

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
+
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -12,31 +15,88 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS users (
+                id BIGINT PRIMARY KEY AUTO_INCREMENT, 
+                name VARCHAR (50),
+                lastname VARCHAR(50),
+                age TINYINT
+                )
+                """;
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+                
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
 
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
 
+            User user = new User(name, lastName, age);
+            session.save(user);
+
+            session.getTransaction().commit();
+            System.out.println("Пользователь с именем – " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
 
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // set up a database connection
+    // JDBC
     private static final String url = "jdbc:mysql://localhost:3306/ProjectHabsida";
     private static final String user = "root";
     private static final String password = "Mfghost66";
@@ -16,5 +22,33 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка при подключении к БД", e);
         }
+    }
+
+    //Hibernate
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                configuration.setProperty("hibernate.connection.url", url);
+                configuration.setProperty("hibernate.connection.username", user);
+                configuration.setProperty("hibernate.connection.password", password);
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+                configuration.setProperty("hibernate.show_sql", "true");
+
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration
+                        .buildSessionFactory(new StandardServiceRegistryBuilder()
+                                .applySettings(configuration.getProperties())
+                                .build());
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Ошибка создания SessionFactory");
+            }
+        }
+        return sessionFactory;
     }
 }
